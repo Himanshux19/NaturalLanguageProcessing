da5193cbcb9f62ed144035044d777c5d75b9acaa
Himanshu Singh <himanshu124singh19@gmail.com>
2026-04-04 00:13:31 +0530

    Added Dockerfile

--- src/main/java/com/nlp/NLP/WebConfig.java
+++ src/main/java/com/nlp/NLP/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:63342")
+                .allowedOrigins("https://nlpextractor.netlify.app/")
                 .allowedMethods("POST")
                 .allowedHeaders("*")
                 .allowCredentials(true);
