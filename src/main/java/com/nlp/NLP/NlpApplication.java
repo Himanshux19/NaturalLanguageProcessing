223794cd945a6cf31259b47933a1813951720256
Himanshu Singh <himanshu124singh19@gmail.com>
2026-04-03 16:48:06 +0530

    Created NERController and Pipeline

--- src/main/java/com/nlp/NLP/NlpApplication.java
+++ src/main/java/com/nlp/NLP/NlpApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NlpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NlpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NlpApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/nlp/NLP/controller/NERController.java
@@ -0,0 +1,41 @@
+package com.nlp.NLP.controller;
+
+import com.nlp.NLP.model.Type;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1")
+public class NERController {
+
+    @Autowired
+    private StanfordCoreNLP stanfordCoreNLP;
+
+    @PostMapping
+    @RequestMapping(value = "/ner")
+    public Set<String> ner(@RequestBody final String input, @RequestParam final Type type) {
+
+        CoreDocument document = new CoreDocument(input);
+        stanfordCoreNLP.annotate(document);
+        List<CoreLabel> coreLabels = document.tokens();
+        return new HashSet<>(collectList(coreLabels,type));
+    }
+
+    private List<String> collectList (List<CoreLabel> coreLabels,final Type type) {
+
+        return coreLabels
+                .stream()
+                .filter(coreLabel -> type.getName().equalsIgnoreCase(coreLabel.get(CoreAnnotations.NamedEntityTagAnnotation.class)))
+                .map(CoreLabel::originalText)
+                .collect(Collectors.toList());
+    }
+}
